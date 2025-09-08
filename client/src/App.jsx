4a185e40e151da8c2eb44a8ca9c927faf89a01a0
umairafatima29 <umairafatima897@gmail.com>
2025-09-08 18:07:50 +0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Users from "./Users";
+import CreateUser from "./CreateUser";
+import UpdateUser from "./UpdateUser";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <BrowserRouter>   
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/create" element={<CreateUser />} />
+          <Route path="/update/:id" element={<UpdateUser />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
